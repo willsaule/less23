7d19fbaccf655077b19d601f27b13862e8182e77
willsaule <avasjashasynov@gmail.com>
2023-04-04 09:45:32 +0600

    first commit

---
+++ less24.py
@@ -0,0 +1,73 @@
+# lst = [1,2,3]
+# lst.extend([4,5,6])
+# lst.append(7)
+# print(lst)
+
+# lst_new = [1,2,3]
+# lst_new.insert(1,4)
+# print(lst_new)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# lst = [i for i in 'Yasin']
+# print("".join(lst))
+
+# fruit_lst = ['apple' , 'banan' , 'cherry']
+# fruit_lst.append('pear')
+# print(fruit_lst)
+
+# games_lst = ['Dota' , 'PUBG']
+# games_lst.extend (['Free Fire' , 'Minecraft' , 'Mobilelgends'])
+# print(games_lst)
+
+# python_group = ['Marsel' , 'Aman' , 'Akyl' , 'Saule' , 'Nursalam']
+# print(python_group.index('Yasin'))
+
+# telephone_lst = ['Iphone','Samsung','Nokia','Banan']
+# remove_el = telephone_lst.pop()
+# print(remove_el)
+
+# numbers_lst = [1,3,4,5,6,7,8,9,10]
+
+# lst = [12,14,15,16,66,777,87,34,34,234,123]
+# print(max(lst))
+
+# lst = [12,14,15,16,66,777,87,34,34,234,123]
+# small = lst[0]
+# for num in lst:
+#      if num > small:
+#          small = num
+# print(small)
+
+
+# lst = [12,14,15,16,66,777,87,34,34,234,123]
+# small = lst[0]
+# for num in lst:
+#      if num < small:
+#          small = num
+# print(small)
